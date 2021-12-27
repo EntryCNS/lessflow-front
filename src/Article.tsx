0d273e110a2d2ad304b318324c0a769243e7530d
강성훈 <80798929+ksh5324@users.noreply.github.com>
2021-12-27 11:09:01 +0900

    feat: upload code

--- src/Article.tsx
+++ src/Article.tsx
@@ -1,53 +1,77 @@
-import {interpolate, Sequence, useCurrentFrame, useVideoConfig} from 'remotion';
+import {
+  interpolate,
+  Sequence,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
 
-type sentence = {id:number, content: string, duration: number}
+type sentence = { id: number; content: string; duration: number };
 function getStartTime(sentences: Array<sentence>, item: sentence) {
-	const idx = sentences.indexOf(item)
+  const idx = sentences.indexOf(item);
 
-	let time = 0
-	for(let i = 0; i < idx; i++) {
-		console.log(`${sentences[i].content}(${sentences[i].duration}초)`)
-		time += sentences[i].duration
-	}
+  let time = 0;
+  for (let i = 0; i < idx; i++) {
+    console.log(`${sentences[i].content}(${sentences[i].duration}초)`);
+    time += sentences[i].duration;
+  }
 
-	return time
+  return time;
 }
 
 function sumSentenceTime(sentences: Array<sentence>) {
-	let time = 0
-	sentences.forEach((it) => time += it.duration)
-	return time
+  let time = 0;
+  sentences.forEach((it) => (time += it.duration));
+  return time;
 }
 
-const TIME_PADDING = 0.7
+const TIME_PADDING = 0.7;
 const Article: React.FC<{
-	news: {
-		title: string,
-		sentences: Array<sentence>
-	}
-}> = ({news}) => {
-	const frame = useCurrentFrame();
-	const videoConfig = useVideoConfig()
-	const TIMEPADDING_FORFRAME = TIME_PADDING * videoConfig.fps
-	console.log(news.sentences)
+  news: {
+    title: string;
+    sentences: Array<sentence>;
+  };
+}> = ({ news }) => {
+  const frame = useCurrentFrame();
+  const videoConfig = useVideoConfig();
+  const TIMEPADDING_FORFRAME = TIME_PADDING * videoConfig.fps;
+  console.log(news.sentences);
 
-	return (
-		<div style={{flex: 1, backgroundColor: 'white'}}>
-				<Sequence from={TIMEPADDING_FORFRAME} durationInFrames={Math.floor(videoConfig.fps * (sumSentenceTime(news.sentences)))}>
-					<h1 style={{ width:'100%', textAlign: 'center', fontSize: '64px' }}>{news.title}</h1>
-				</Sequence>
-				{
-					news.sentences.map(it => {
-						return (
-						<Sequence from={TIMEPADDING_FORFRAME + Math.round(getStartTime(news.sentences, it) * videoConfig.fps)}
-							durationInFrames={Math.round(videoConfig.fps * it.duration)}>
-							<h3 style={{ width: '50%', fontSize: '48px', marginTop:'200px', height: '100%' }}>{it.content}</h3>
-						</Sequence>
-						)
-					})
-				}
-		</div>
-	);
+  return (
+    <div style={{ flex: 1, backgroundColor: "white" }}>
+      <Sequence
+        from={TIMEPADDING_FORFRAME}
+        durationInFrames={Math.floor(
+          videoConfig.fps * sumSentenceTime(news.sentences)
+        )}
+      >
+        <h1 style={{ width: "100%", textAlign: "center", fontSize: "64px" }}>
+          {news.title}
+        </h1>
+      </Sequence>
+      {news.sentences.map((it) => {
+        return (
+          <Sequence
+            from={
+              TIMEPADDING_FORFRAME +
+              Math.round(getStartTime(news.sentences, it) * videoConfig.fps)
+            }
+            durationInFrames={Math.round(videoConfig.fps * it.duration)}
+          >
+            <h3
+              style={{
+                width: "50%",
+                fontSize: "48px",
+                marginTop: "200px",
+                height: "100%",
+              }}
+            >
+              {it.content}
+            </h3>
+          </Sequence>
+        );
+      })}
+    </div>
+  );
 };
 
-export { sumSentenceTime, sentence, Article }
+export { sumSentenceTime, sentence, Article };
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,34 +1,34 @@
-import { Composition } from 'remotion';
-import { Article, sumSentenceTime } from './Article'
-import data from './newsData'
+import { Composition } from "remotion";
+import { Article, sumSentenceTime } from "./Article";
+import data from "./newsData";
 
-const frame = 30
+const frame = 30;
 
-const newsTimeMap = new Map<string, number>()
+const newsTimeMap = new Map<string, number>();
 data.forEach((news) => {
-	newsTimeMap.set(news.title, sumSentenceTime(news.sentences))
-})
+  newsTimeMap.set(news.title, sumSentenceTime(news.sentences));
+});
 
 export const RemotionVideo: React.FC = () => {
-	return (
-		<>
-			{
-				data.map((news) => {
-					return (
-						<Composition
-							id={`article${data.indexOf(news) + 1}`}
-							component={Article}
-							durationInFrames={Math.floor(frame * (newsTimeMap.get(news.title)!! + 1.5))}
-							fps={frame}
-							width={1920}
-							height={1080}
-							defaultProps={{
-								news
-							}}
-						/>
-					)
-				})
-			}
-		</>
-	);
+  return (
+    <>
+      {data.map((news) => {
+        return (
+          <Composition
+            id={`article${data.indexOf(news) + 1}`}
+            component={Article}
+            durationInFrames={Math.floor(
+              frame * (newsTimeMap.get(news.title)!! + 1.5)
+            )}
+            fps={frame}
+            width={1920}
+            height={1080}
+            defaultProps={{
+              news,
+            }}
+          />
+        );
+      })}
+    </>
+  );
 };
